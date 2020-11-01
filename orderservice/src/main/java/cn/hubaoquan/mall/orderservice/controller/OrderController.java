9bde9c8eefce5fad02ffd40c094bd0600277fcf4
hubaoquan <971281722@qq.com>
2020-11-01 19:36:44 +0800

    init

--- orderservice/src/main/java/cn/hubaoquan/mall/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/cn/hubaoquan/mall/orderservice/controller/OrderController.java
@@ -1,21 +1,15 @@
 package cn.hubaoquan.mall.orderservice.controller;
-
-
-import cn.hubaoquan.mall.orderservice.OrderserviceApplication;
 import cn.hubaoquan.mall.orderservice.mapper.OrderDetailMapper;
 import cn.hubaoquan.mall.orderservice.mapper.OrderMapper;
 import cn.hubaoquan.mall.orderservice.model.Order;
 import cn.hubaoquan.mall.orderservice.model.OrderDetail;
 import cn.hubaoquan.mall.productservice.model.Product;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import javax.annotation.Resource;
-import java.util.UUID;
 
 @RequestMapping("/order")
 @RestController
@@ -29,12 +23,12 @@ public class OrderController {
     RestTemplate restTemplate;
 
     @RequestMapping("/create")
-    public String createOrder(@RequestParam Integer userId,@RequestParam Integer productId,@RequestParam Integer quantity)
-    {
-        String url = "http://product-server/product/"+productId;
-        Product product= restTemplate.getForObject(url, Product.class);
+    public String createOrder(@RequestParam Integer userId, @RequestParam Integer productId, @RequestParam Integer quantity) {
+        String url = "http://product-server/product/" + productId;
+        Product product = restTemplate.getForObject(url, Product.class);
         Order order = new Order();
-        order.setTotal(product.getPrice()*quantity);
+        if (product!=null)
+        order.setTotal(product.getPrice() * quantity);
         order.setUserId(userId);
         orderMapper.insert(order);
         OrderDetail orderDetail = new OrderDetail();
