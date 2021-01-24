bdefe3c8f269236a7430e4abc85893ecfa0e15c2
hubaoquan <971281722@qq.com>
2021-01-24 18:33:28 +0800

    hystrix

--- orderservice/src/main/java/cn/hubaoquan/mall/orderservice/OrderserviceApplication.java
+++ orderservice/src/main/java/cn/hubaoquan/mall/orderservice/OrderserviceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients //使用Feign
 @EnableCircuitBreaker //使用hystrix
+@EnableHystrixDashboard // 使用hystrix 仪表盘
 @MapperScan("cn.hubaoquan.mall.orderservice.mapper")
 public class OrderserviceApplication {
 
